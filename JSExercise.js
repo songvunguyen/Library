3c8d223c419b7f2c331b2d493ef7c4f79bccd024
Song Vu Nguyen <vunnems08@gmail.com>
2020-12-17 12:11:55 -0500

    Finish but can improve

--- JSExercise.js
+++ JSExercise.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
+    this.index = myLibrary.length-1;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -14,35 +15,60 @@ function addBookToLibrary(book) {
 }
 
 function displayBook() {
+    //remove old card if exist
+    let cards = document.querySelectorAll(".card");
+    if(cards != undefined){
+        cards.forEach((card) => {
+            card.remove();
+        });
+    }
     for (const book of myLibrary) {
         let temp = document.createElement('div');
         let p = document.createElement("pre");
         p.textContent = `Title: ${book.title}\nAuthor: ${book.author}\nPages: ${book.pages}\nRead: ${book.read}`;
+        let r = document.createElement('button');
+        r.textContent = "Remove";
+        r.classList.add("remove");
+        r.addEventListener("click", () => {removeBook(book)});
         temp.appendChild(p);
+        temp.appendChild(r);
         temp.classList.add("card");
         temp.style.cssText = "display: inline-block";
-        document.querySelector("#container").appendChild(temp);
+        document.querySelector("#container").appendChild(temp);       
     }
 }
 
-let one = new Book("hi", "hello", 200, false);
-let two = new Book("hi", "hello", 200, false);
-let three = new Book("hi", "hello", 200, false);
+//Add new book to library based on the submited form info
+function submitForm(form){
+    let book = new Book(form.elements['title'].value, form.elements['author'].value, form.elements['pages'].value, form.elements['read'].checked);
+    addBookToLibrary(book);
+    form.reset();
+    form.style.display = "none";
+    displayBook();
+}
 
-addBookToLibrary(one);
-addBookToLibrary(two);
-addBookToLibrary(three);
+//Remove book from library
+function removeBook(book){
+    myLibrary.splice(book.index,1);
+    displayBook();
+}
 
+// let one = new Book("hi", "hello", 200, false);
+// let two = new Book("hi", "hello", 200, false);
+// let three = new Book("hi", "hello", 200, false);
+
+// addBookToLibrary(one);
+// addBookToLibrary(two);
+// addBookToLibrary(three);
+
+//Display form when new book button click on
 let newBook = document.querySelector("#new");
 newBook.addEventListener('click', function () {
     document.querySelector('#newBook').style.display = "block";
 });
 
+//Add event listener to submit button
 let submit = document.querySelector("#submit");
-submit.addEventListener('click', function () {
-    let form = document.querySelector("#newBook").elements;
-    let book = new Book(form[0], form[1], form[2], form[3]);
-    addBookToLibrary(book);
-    document.querySelector('#newBook').style.display = "none";
-    displayBook();
-});
+submit.addEventListener('click', () => {submitForm(document.querySelector("#newBook"))});
+   
+displayBook();
